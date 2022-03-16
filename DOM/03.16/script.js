8836aef6737cbc4ed5297ac3f60eaff4d5304f76
Taichir0923 <54131730+Taichir0923@users.noreply.github.com>
2022-03-16 11:40:38 +0800

    Update script.js

--- DOM/03.16/script.js
+++ DOM/03.16/script.js
@@ -2,6 +2,7 @@
 
 const img = document.querySelector('#img');
 const avatar = document.querySelector('#avatar');
+const loader = document.querySelector('.loader');
 
 // addEventListener(event , callback);
 // onchange , oninput
@@ -9,7 +10,13 @@ const avatar = document.querySelector('#avatar');
 
 img.addEventListener('change' , function(){
     const image = img.files[0];
-    avatar.src = URL.createObjectURL(image);
+    loader.classList.remove('hide');
+    loader.classList.add('show');
+    setTimeout(function(){
+        avatar.src = URL.createObjectURL(image);
+        loader.classList.remove('show');
+        loader.classList.add('hide');
+    } , 3000)
 });
 
 // state
